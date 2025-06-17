1b714014c981b68dca6a59724124741cb01c9a43
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-06-17 16:08:05 -0300

    Renamed Streams Class To CreatingStreams
    - Renamed For Better Clarity Regarding The File's Purpose;
    - Added Javadocs And Inline Comments;

--- src/br/com/eaugusto/CreatingStreams.java
+++ src/br/com/eaugusto/CreatingStreams.java
@@ -6,21 +6,32 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 /**
+ * Demonstrates various ways to create Streams in Java. Shows how to create
+ * streams from List, Set, Map keys and values, as well as directly from
+ * Stream.of().
+ * 
  * @author Eduardo Augusto (github.com/AsrielDreemurrGM/)
  * @since Jun 15, 2025
  */
-public class Streams {
+public class CreatingStreams {
 
 	public static void main(String[] args) {
+		// Creates a Stream from a List of Strings.
 		Stream<String> stream = List.of("Eduardo", "Augusto", "Test").stream();
 
+		// Creates a Stream directly from values using Stream.of().
 		Stream<Integer> streamInt = Stream.of(1, 2, 3, 4);
 
+		// Creates a Stream from a Set of Strings.
 		Stream<String> set = Set.of("Eduardo", "Augusto", "Test").stream();
 
+		// Creates a Map with one key-value pair.
 		Map<String, String> map = Map.of("Eduardo", "Augusto");
-		Stream<String> mapKeys = map.keySet().stream();
-		Stream<String> mapValues = map.values().stream();
 
+		// Creates a Stream from the keys of the Map.
+		Stream<String> mapKeys = map.keySet().stream();
+
+		// Creates a Stream from the values of the Map.
+		Stream<String> mapValues = map.values().stream();
 	}
 }
