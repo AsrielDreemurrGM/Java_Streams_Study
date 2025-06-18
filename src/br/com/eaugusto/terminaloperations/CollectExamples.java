f4a8fbd65b073566a53b7b45fe8d429c610447a8
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-06-18 19:48:49 -0300

    Added Javadocs And Inline Comments
    - Renamed CollectExample To CollectExamples;
    - Added Javadocs And Inline Comments To The CollectExamples And OptionalExamples Classes;

--- src/br/com/eaugusto/terminaloperations/CollectExamples.java
+++ src/br/com/eaugusto/terminaloperations/CollectExamples.java
@@ -9,15 +9,22 @@ import java.util.stream.Collectors;
 import br.com.eaugusto.Person;
 
 /**
+ * Demonstrates the use of the collect() terminal operation in Java Streams. The
+ * collect() method is used to transform the elements of a stream into different
+ * data structures such as List, Set, ArrayList, and Map.
+ * 
  * @author Eduardo Augusto (github.com/AsrielDreemurrGM/)
  * @since Jun 17, 2025
  */
-public class CollectExample {
+public class CollectExamples {
 
 	public static void main(String[] args) {
+
+		// Creates and populates a list of Person objects.
 		List<Person> list = new Person().populatePeople();
 
 		System.out.println("List Of All Brazilians Using Collect And Saving Them In A New List");
+		// Collects all Brazilians into a new List.
 		List<Person> listOfBrazilians = list.stream()
 				.filter(person -> person.getNationality().equals("Brazil"))
 				.collect(Collectors.toList());
@@ -25,12 +32,14 @@ public class CollectExample {
 		listOfBrazilians.forEach(System.out::println);
 
 		System.out.println("\nList Of All Brazilians Printed Directly Without Saving A New List");
-
+		// Collects Brazilians and prints them directly without saving the list.
 		list.stream()
-				.filter(person -> person.getNationality().equals("Brazil")).collect(Collectors.toList())
-				.forEach(System.out::println);
+		.filter(person -> person.getNationality().equals("Brazil"))
+		.collect(Collectors.toList())
+		.forEach(System.out::println);
 
 		System.out.println("\nSet Of All Brazilians Using Collect And Saving Them In A Set");
+		// Collects all Brazilians into a Set to eliminate duplicates.
 		Set<Person> set = list.stream()
 				.filter(person -> person.getNationality().equals("Brazil"))
 				.collect(Collectors.toSet());
@@ -38,25 +47,28 @@ public class CollectExample {
 		set.forEach(System.out::println);
 
 		System.out.println("\nArray List Of All Canadians Using Collect And Saving Them In A New ArrayList");
+		// Collects all Canadians into a new ArrayList.
 		ArrayList<Person> arrayList = list.stream()
 				.filter(person -> person.getNationality().equals("Canada"))
 				.collect(Collectors.toCollection(ArrayList::new));
 
 		arrayList.forEach(System.out::println);
-	
+
 		System.out.println("\nMap Of All Mexicans Using Collect And Saving Them In A Map");
+		// Collects all Mexicans into a Map using their ID as key and a new Person as value.
 		Map<String, Object> map = list.stream()
 				.filter(person -> person.getNationality().equals("Mexico"))
 				.collect(Collectors.toMap(Person::getId, Person::new));
 
-		map.forEach((key, value ) -> System.out.println("Map Key (Person Id): " + key + " | "
-		+ "Map Value (Person object): " + value));
-	
+		map.forEach((key, value) -> System.out.println("Map Key (Person Id): " + key + " | "
+				+ "Map Value (Person object): " + value));
+
 		System.out.println("\nMap Of Everyone Grouped By Age");
+		// Groups all persons by age, resulting in a Map where the key is age and value is list of people.
 		Map<Integer, List<Person>> groupingByAge = list.stream()
 				.collect(Collectors.groupingBy(Person::getAge));
 
-		groupingByAge.forEach((key, value ) -> System.out.println("Map Key (Person Age): " + key + " | " + "Map Value (Person object): " + value));
-		
+		groupingByAge.forEach((key, value) -> System.out.println("Map Key (Age): " + key + " | "
+				+ "Map Value (List of Persons): " + value));
 	}
 }
--- src/br/com/eaugusto/terminaloperations/OptionalExamples.java
+++ src/br/com/eaugusto/terminaloperations/OptionalExamples.java
@@ -7,6 +7,10 @@ import java.util.Optional;
 import br.com.eaugusto.Person;
 
 /**
+ * Demonstrates the use of Optional with terminal operations max() and min().
+ * Shows how to retrieve the oldest and youngest persons from a list using
+ * Streams.
+ * 
  * @author Eduardo Augusto (github.com/AsrielDreemurrGM/)
  * @since Jun 18, 2025
  */
@@ -14,20 +18,24 @@ public class OptionalExamples {
 
 	public static void main(String[] args) {
 
+		// Creates and populates a list of Person objects.
 		List<Person> list = new Person().populatePeople();
 
-		// If two people happen to have the same age it returns the first that was found
-		// on the list
 		System.out.println("Getting The Oldest Person From The List");
+		// Finds the person with the maximum age using Comparator.comparing.
+		// If multiple people have the same age, returns the first match found.
 		Optional<Person> oldestPerson = list.stream().max(Comparator.comparing(Person::getAge));
 
+		// Checks if the result is present and prints it.
 		if (oldestPerson.isPresent()) {
 			System.out.println(oldestPerson.get());
 		}
 
 		System.out.println("Getting The Youngest Person From The List");
+		// Finds the person with the minimum age using Comparator.comparing.
 		Optional<Person> youngestPerson = list.stream().min(Comparator.comparing(Person::getAge));
 
+		// Checks if the result is present and prints it.
 		if (youngestPerson.isPresent()) {
 			System.out.println(youngestPerson.get());
 		}
